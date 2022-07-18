e97e04087ea4cbb6f02cc92c46c8d18d67209fd9
nedcrow <nedcrow0@gmail.com>
2022-07-18 11:29:30 +0900

    Test: Selfie segmentation with python.
    Add unreal python plugin & python script(use mediapipe, cv2, urllib, etc..).
    Change folder name(textures -> Textures).
    Create chroma key material instance from segmented image.

--- Source/TestMobileChromakey/TestMobileChromakey.Build.cs
+++ Source/TestMobileChromakey/TestMobileChromakey.Build.cs
@@ -8,13 +8,13 @@ public class TestMobileChromakey : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP", "Json", "MobileHelper" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP", "Json", "MobileHelper", "UnrealEd", "Blutility", "ContentBrowser", "AssetRegistry", "Python3", "PythonScriptPlugin" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "PythonScriptPlugin" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
